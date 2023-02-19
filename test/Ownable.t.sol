c5db8e1dcc5bd9964741693a9985309a9a0a5042
Enrique Ortiz <hi@enriqueortiz.dev>
2023-02-19 17:57:07 -0400

    feat: add ownable tests

---
+++ test/Ownable.t.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.15;
+
+import {GrimoireTest} from "./GrimoireTest.sol";
+import {MockOwnable} from "./mocks/MockOwnable.sol";
+
+contract OwnableTest is GrimoireTest {
+    /*//////////////////////////////////////////////////////////////
+                            EVENT STORAGE
+    //////////////////////////////////////////////////////////////*/
+    MockOwnable mock;
+
+    /*//////////////////////////////////////////////////////////////
+                            TEST CASES
+    //////////////////////////////////////////////////////////////*/
+    error NotOwner(address owner, address caller);
+
+    /*//////////////////////////////////////////////////////////////
+                                SETUP
+    //////////////////////////////////////////////////////////////*/
+    function setUp() public {
+        mock = new MockOwnable();
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                              UNIT TESTS
+    //////////////////////////////////////////////////////////////*/
+
+    function test_transferOwnership() public {
+        address newOwner = address(0xDEADBEEF);
+        mock.transferOwnership(newOwner);
+        assertEq(mock.owner(), newOwner);
+    }
+
+    function test_callWhileNotOwner() public {
+        address notOwner = address(0xDEADBEEF);
+        vm.prank(notOwner);
+
+        vm.expectRevert(abi.encodeWithSignature("NotOwner(address,address)", address(this), notOwner));
+        mock.updateLock();
+    }
+
+    function test_callWhileOwner() public {
+        mock.updateLock();
+        assertTrue(mock.lock());
+    }
+}
