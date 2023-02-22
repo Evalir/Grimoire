6d602df0477631f9ea592f3204b35231dcd9430f
Enrique Ortiz <hi@enriqueortiz.dev>
2023-02-21 22:35:59 -0400

    chore: add comments, change to proper style

--- src/utils/BytesDataLib.sol
+++ src/utils/BytesDataLib.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
+/// @title BytesDataLib
+/// @author Enrique Ortiz @Evalir
+/// @notice Easily convert from an arbitrary amount of bytes to another type.
 library BytesDataLib {
     function bytesToAddress(bytes memory data) internal pure returns (address) {
         return address(uint160(uint256(bytes32(data))));
